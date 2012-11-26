a1d0edb513fa4f13914101f63bca84cbdcc8571e
nielsdoorn <nielsdoorn@gmail.com>
2012-11-26 22:06:15 +0100

    cool

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 var http = require("http");
 var url = require("url");
 
+
+
 function start(route, handle) {
+	var port = process.env.PORT;
+	if (port === undefined) {
+		port = 8888;
+	}
+
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
     route(handle, pathname, response, request);
   }
 
-  http.createServer(onRequest).listen(8888);
-  console.log("Server has started.");
+  http.createServer(onRequest).listen(port);
+  console.log("Server has started on port "+port);
 }
 
 exports.start = start;
